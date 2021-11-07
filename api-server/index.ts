cb6199596fe2d5706f24f6d2040dd7050486419e
白鳥貴士 <tks.srtr89@gmail.com>
2021-11-08 02:58:27 +0900

    update  api-server/index.ts

--- api-server/index.ts
+++ api-server/index.ts
@@ -3,18 +3,28 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 async function main() {
-	await prisma.user.create({
-		data: {
-			name: 'Alice',
-			email: 'alice@prisma.io',
-			posts: {
-				create: { title: 'Hello World' },
-			},
-			profile: {
-				create: { bio: 'I like turtles' },
-			},
+	const targetUser = {
+		name: 'Alice',
+		email: 'alice@prisma.io',
+		posts: {
+			create: { title: 'Hello World' },
+		},
+		profile: {
+			create: { bio: 'I like turtles' },
+		},
+	};
+
+	const findTargetUserResult = await prisma.user.findUnique({
+		where: {
+			email: targetUser.email,
 		},
 	});
+
+	if (!findTargetUserResult) {
+		await prisma.user.create({
+			data: targetUser,
+		});
+	}
 
 	const allUsers = await prisma.user.findMany({
 		include: {
@@ -22,6 +32,7 @@ async function main() {
 			profile: true,
 		},
 	});
+
 	console.dir(allUsers, { depth: null });
 }
 
